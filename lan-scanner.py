c01795463d521e143d3848fe5e129c0aa6af3b3a
renato.sirola@gmail.com <renato.sirola@gmail.com>
2025-02-15 20:32:39 +0000

    lan scanner - first working tentative

--- lan-scanner.py
+++ lan-scanner.py
@@ -1,8 +1,12 @@
+import socket
 import subprocess
 import ipaddress
 import threading
+import time
+from queue import Queue
 
-# need to be finished
+scan_result = Queue()
+threads = []
 
 def show_banner():
     banner = """
@@ -17,44 +21,66 @@ by m1ch3al\n\n
     """
     print(banner)
 
+
+def get_dns_by_ip(ip_address):
+    try:
+        host_name, alias_list, ip_list = socket.gethostbyaddr(ip_address)
+        return host_name  # Restituisce il nome DNS
+    except socket.herror as e:
+        return None
+
+
 def ping(address):
+    global scan_result
     try:
         result = subprocess.run(["ping", "-c", "2", address], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                 text=True)
         if result.returncode == 0:
-            print("ACTIVE : {}".format(address))
+            hostname = get_dns_by_ip(address)
+            if hostname is not None:
+                scan_result.put_nowait("{} | ({})".format(address, hostname))
+            else:
+                scan_result.put_nowait("{}".format(address))
             return True
         else:
             return False
     except Exception as e:
         return False
 
-def scan_lan(network):
-    try:
-        # Usa il modulo ipaddress per analizzare la sottorete (es. 192.168.1.0/24)
-        net = ipaddress.ip_network(network, strict=False)
-        print(f"[INFO] Inizio scansione della rete: {network}")
 
-        # Scansiona ogni indirizzo IP nella rete
-        active_hosts = []
-        for ip in net.hosts():  # Esclude gli indirizzi broadcast e network
+def scan_lan(network):
+    global threads
+    try:
+        net = ipaddress.ip_network(network, strict=False)
+        print("Starting LAN-Scan into: {}\nPLEASE WAIT...".format(network))
+        for ip in net.hosts():
             ip_str = str(ip)
             ping_thread = threading.Thread(target=ping, args=(ip_str,))
             ping_thread.daemon = True
+            threads.append(ping_thread)
             ping_thread.start()
-        #print(f"\n[INFO] Scansione completata. Host attivi trovati: {len(active_hosts)}")
-        #return active_hosts
     except ValueError as e:
-        print(f"Errore nella rete specificata: {e}")
-        return []
+        print("Error in the specified network: {}".format(e))
 
+
+def show_results(lan_information):
+    print("\n[LAN-SCAN completed]")
+    print("Reachable network object(s): {}".format(lan_information.qsize()))
+    while not lan_information.empty():
+        reachable_item = lan_information.get_nowait()
+        print(" + {}".format(reachable_item))
+    print("\n")
 
 def main():
+    global threads
+    global scan_result
     network = input("Insert subnet to scan (es. 192.168.1.0/24): ")
-    active_hosts = scan_lan(network)
-    #print("\nHost attivi trovati:")
-    #for host in active_hosts:
-    #    print(host)
+    scan_lan(network)
+    time.sleep(2)
+    for thread in threads:
+        thread.join()
+    show_results(scan_result)
+
 
 if __name__ == "__main__":
     main()
